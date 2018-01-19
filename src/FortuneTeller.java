75587ac4e957348e4b0cddd1f7a0b3f8fa67e5ae
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-01-19 10:13:46 -0500

    Set variables to inputs

--- src/FortuneTeller.java
+++ src/FortuneTeller.java
@@ -1,21 +1,22 @@
 import java.util.Scanner;
+
 public class FortuneTeller {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
 		System.out.print("Enter your first name: ");
-		String firstName;
+		String firstName = input.next();
 		System.out.print("Enter your last name: ");
-		String lastName;
+		String lastName = input.next();
 		System.out.print("Enter your age: ");
-		int age;
+		int age = input.nextInt();
 		System.out.print("Enter your birth month (1-12): ");
-		int month;
+		int month = input.nextInt();
 		System.out.print("Enter your favorite color: ");
-		String color;
+		String color = input.next();
 		System.out.print("How many siblings do you have? ");
-		int siblings;
+		int siblings = input.nextInt();
 		
 		input.close();
 	}
