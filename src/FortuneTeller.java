575b80b642a544d7c434a9064a9a90d2cdd3dbf3
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-01-19 10:11:05 -0500

    Added console prompts

--- src/FortuneTeller.java
+++ src/FortuneTeller.java
@@ -3,12 +3,20 @@ public class FortuneTeller {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter your first name: ");
 		String firstName;
+		System.out.print("Enter your last name: ");
 		String lastName;
+		System.out.print("Enter your age: ");
 		int age;
+		System.out.print("Enter your birth month (1-12): ");
 		int month;
+		System.out.print("Enter your favorite color: ");
 		String color;
+		System.out.print("How many siblings do you have? ");
 		int siblings;
+		
 		input.close();
 	}
 
