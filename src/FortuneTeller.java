d15e1f9ea31c9d260ea137e3b6c6a81c532d6f2f
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-01-19 10:07:19 -0500

    Declared variables

--- src/FortuneTeller.java
+++ src/FortuneTeller.java
@@ -3,6 +3,12 @@ public class FortuneTeller {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		String firstName;
+		String lastName;
+		int age;
+		int month;
+		String color;
+		int siblings;
 		input.close();
 	}
 
